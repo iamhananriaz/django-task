5ccd5a139cfcf0f45168b56bd9c77dd8d2c2ddb4
Hanan <thehananriaz@gmail.com>
2024-06-26 20:53:36 +0500

    template engine and exclude

--- hello/views.py
+++ hello/views.py
@@ -2,25 +2,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
 
 
-def signup(request):
-    if request.method == 'post':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = SignUpForm()
-    return render(request, 'forms/SignUpForm.html', {'form': form})
+
 
 def home(request):
-    return render(request, 'home.html')
+    people = [
+        {'name' : 'Loki',  'location': 'azguard'},
+        {'name' : 'Thor', 'location': 'Newyork'},
+        {'name' : 'Steve', 'location': 'Afganistan'}
+    ]
 
-def blog(request):
-    return render(request, 'blogs.html')
+ 
+    return render(request, 'home.html', context = {'people': people })
+
+def about(request): 
+    return render(request, 'about.html')
+
+def contact(request): 
+    return render(request, 'contact.html')
